bed3e375a740d577d79b10c1f93abf27cbd65810
Bradley Hill <brad.michael.hill@gmail.com>
2024-08-27 12:17:23 +0200

    Refactor EducationInfo component to use form submission and enable edit button.

--- src/components/EducationInfo.tsx
+++ src/components/EducationInfo.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Button from "./Button";
+import React from "react";
+
 import EditButton from "./EditButton";
 import SubmitButton from "./SubmitButton";
 import "../styles/EducationInfo.css";
 
 export default function EducationInfo() {
-
   const [isEditing, setIsEditing] = React.useState(true);
   const [school, setSchool] = React.useState("");
   const [course, setCourse] = React.useState("");
@@ -44,57 +43,81 @@ export default function EducationInfo() {
     setIsEditing(true);
   };
   return (
-
-    <section><h2>Education Information</h2>
-    {isEditing ? (
-
-    <form onSubmit={handleSubmit}>
-      <fieldset>
-      <legend>Education History</legend>
-      <div className="formGroup">
-      <label htmlFor="schoolInput">School: </label>
-      <input type="text" id="schoolInput" name='school' onChange={handleInputChange}/>
-
-      </div>
-      <div className="formGroup">
-      <label htmlFor="courseInput">Title of Course: </label>
-      <input type="text" id="courseInput" name='course' onChange={handleInputChange}/>
-
-      </div>
-      <div className="formGroup">
-      <label htmlFor="startInput">Start Date: </label>
-      <input type="date" id="startInput" name='startDate' onChange={handleInputChange}/>
-
-      </div>
-      <div className="formGroup">
-      <label htmlFor="endInput">End Date: </label>
-      <input type="date" id="endInput" name='endDate' onChange={handleInputChange}/>
-
-      </div>
-
-      </fieldset>
-      <div className="buttonContainer">
-        <SubmitButton>Submit</SubmitButton>
-        <EditButton onClick={handleEdit}>Edit</EditButton>
-        <Button />
-        <Button />
-      </div>
-    </form>
-    ) : (
-      <div className="displayEducation">
-        <p><strong>School: </strong> {school}</p>
-        <p><strong>Course: </strong>{course}</p>
-        <p><strong>Start Date: </strong>{startDate}</p>
-        <p><strong>End Date: </strong>{endDate}</p>
-        <div className="buttonContainer">
-        <SubmitButton>Submit</SubmitButton>
-        <EditButton onClick={handleEdit}>Edit</EditButton>
-        <Button />
-        <Button />
-      </div>
-      </div>
-    )}
+    <section>
+      <h2>Education Information</h2>
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Education History</legend>
+            <div className="formGroup">
+              <label htmlFor="schoolInput">School: </label>
+              <input
+                type="text"
+                id="schoolInput"
+                name="school"
+                value={school}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="courseInput">Title of Course: </label>
+              <input
+                type="text"
+                id="courseInput"
+                name="course"
+                value={course}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="startInput">Start Date: </label>
+              <input
+                type="date"
+                id="startInput"
+                name="startDate"
+                value={startDate}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="endInput">End Date: </label>
+              <input
+                type="date"
+                id="endInput"
+                name="endDate"
+                value={endDate}
+                onChange={handleInputChange}
+              />
+            </div>
+          </fieldset>
+          <div className="buttonContainer">
+            <SubmitButton>Submit</SubmitButton>
+            <EditButton onClick={handleEdit}>Edit</EditButton>
+          </div>
+        </form>
+      ) : (
+        <div className="displayEducation">
+          <p>
+            <strong>School: </strong> {school}
+          </p>
+          <p>
+            <strong>Course: </strong>
+            {course}
+          </p>
+          <p>
+            <strong>Start Date: </strong>
+            {startDate}
+          </p>
+          <p>
+            <strong>End Date: </strong>
+            {endDate}
+          </p>
+          <div className="buttonContainer">
+            <SubmitButton>Submit</SubmitButton>
+            <EditButton onClick={handleEdit}>Edit</EditButton>
+          </div>
+        </div>
+      )}
     </section>
-
-  )
+  );
 }
--- src/components/WorkExperienceInfo.tsx
+++ src/components/WorkExperienceInfo.tsx
@@ -1,38 +1,121 @@
 import React from "react";
-import Button from "./Button";
+
+import EditButton from "./EditButton";
+import SubmitButton from "./SubmitButton";
 import "../styles/WorkExperienceInfo.css";
 
 export default function WorkExperienceInfo() {
+  const [isEditing, setIsEditing] = React.useState(true);
+  const [companyName, setCompanyName] = React.useState("");
+  const [positionTitle, setPositionTitle] = React.useState("");
+  const [startDate, setStartDate] = React.useState("");
+  const [endDate, setEndDate] = React.useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "companyName":
+        setCompanyName(value);
+        break;
+      case "positionTitle":
+        setPositionTitle(value);
+        break;
+      case "startDate":
+        setStartDate(value);
+        break;
+      case "endDate":
+        setEndDate(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+    setIsEditing(false);
+  };
+
+  const handleEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Edit");
+    setIsEditing(true);
+  };
+
   return (
     <section>
       <h2>Work Experience Information</h2>
-      <form>
-        <fieldset>
-          <legend>Work Experience</legend>
-          <div className="formGroup">
-            <label htmlFor="companyNameInput">Company Name: </label>
-            <input type="text" id="companyNameInput" />
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Work Experience</legend>
+            <div className="formGroup">
+              <label htmlFor="companyNameInput">Company Name: </label>
+              <input
+                type="text"
+                id="companyNameInput"
+                name="companyName"
+                value={companyName}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="positionTitleInput">Position Title: </label>
+              <input
+                type="text"
+                id="positionTitleInput"
+                name="positionTitle"
+                value={positionTitle}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="startInput">Start Date: </label>
+              <input
+                type="date"
+                id="startInput"
+                name="startDate"
+                value={startDate}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="endInput">End Date: </label>
+              <input
+                type="date"
+                id="endInput"
+                name="endDate"
+                value={endDate}
+                onChange={handleInputChange}
+              />
+            </div>
+          </fieldset>
+          <div className="buttonContainer">
+            <SubmitButton>Submit</SubmitButton>
+            <EditButton onClick={handleEdit}>Edit</EditButton>
           </div>
-          <div className="formGroup">
-            <label htmlFor="positionTitleInput">Position Title: </label>
-            <input type="text" id="positionTitleInput" />
+        </form>
+      ) : (
+        <div className="displayWorkExperience">
+          <p>
+            <strong>Company Name: </strong> {companyName}
+          </p>
+          <p>
+            <strong>Position Title: </strong> {positionTitle}
+          </p>
+          <p>
+            <strong>Start Date: </strong> {startDate}
+          </p>
+          <p>
+            <strong>End Date: </strong> {endDate}
+          </p>
+          <div className="buttonContainer">
+            <SubmitButton>Submit</SubmitButton>
+            <EditButton onClick={handleEdit}>Edit</EditButton>
           </div>
-          <div className="formGroup">
-            <label htmlFor="startInput">Start Date: </label>
-            <input type="date" id="startInput" />
-          </div>
-          <div className="formGroup">
-            <label htmlFor="endInput">End Date: </label>
-            <input type="date" id="endInput" />
-          </div>
-        </fieldset>
-        <div className="buttonContainer">
-          <Button />
-          <Button />
-          <Button />
-          <Button />
         </div>
-      </form>
+      )}
     </section>
   );
 }
